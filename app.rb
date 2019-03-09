29bd440691254e379d9a410919420eda8b290739
Dmitri Samotessov <dimasamo96@gmail.com>
2019-03-09 23:03:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Dima"
+  end
+
+  get '/hometown' do
+    "My hometown is Netanya."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is What else is there. "
+  end
 end
